867d0c7442956a207b931179722e554a1b45730f
ArielRodriguez07 <a.22308051280706@cbtis128.edu.mx>
2024-09-06 09:36:44 -0600

    Add files via upload

---
+++ miscadena.py
@@ -0,0 +1,20 @@
+nombre="cesar"
+print(nombre[0])
+print(nombre)
+
+MisNovias = ["agripina","anastacia","maria"]
+for chicas in MisNovias:
+    print (chicas)
+    
+print("otro ejemplo")
+b = "Ariel Rodriguez "
+print(b[2:5])
+
+print( "a mayusculas ")
+a = " ariel rodriguez "
+print(a.upper())
+print("concatenar")
+a = "maria"
+b = "perez"
+c = a +"_"+ b
+print(c)
---
+++ misvariables.py
@@ -0,0 +1,17 @@
+
+import math
+edad=65 # 65 se guarda en edad
+nombre="Cesar Reyes Vazquez"
+
+print("tu edad es :",edad)
+print("te llamas :",nombre)
+print("-- tipo de variables --")
+x = str (7) # x will be "3"
+y = int (3.5) # y will be 3'
+z = float (7) 
+print(x)
+print(y)
+print(z)
+print("-- funciones matematicas ----")
+x = math.sqrt(64)
+print(x)
